48acf1f7f9a24e991c623ce58077a3fc52415e2d
Htrope <tropehe12@gmail.com>
2025-10-15 21:14:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Acesso from './components/Acesso'
+import CallToAction from './components/Btn'
+import FeedbackCards from './components/Feedbacks'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+
+function App() {
+  
+
+  return (
+    <div>
+       <Header/>
+       <Hero/>
+       <CallToAction/>
+       <Acesso/>
+       <FeedbackCards/>
+       <CallToAction/>
+    </div>
+   
+   
+  )
+}
+
+export default App
---
+++ src/components/Btn/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function CallToAction() {
+  return (
+    <div className="cta-container">
+      <button className="cta-button">
+        Eu quero tirar <span>900+</span> em Matemática no ENEM
+      </button>
+    </div>
+  );
+}
